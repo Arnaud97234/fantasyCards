c90b34cb711b4f4dc83cd2238833b22d2bbbcc97
Arnaud97234 <arnaud.ulric@gmail.com>
2023-12-11 16:01:23 +0100

    Home page

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -4,11 +4,42 @@ import { useSelector } from 'react-redux'
 function Home() {
     const username = useSelector((state) => state.users.value.username)
 
+
     return (
-    <main className={styles.dashboardContainer}>
-        <h2 className={styles.title}>Dashboard</h2>
-        <p>Welcome {username}</p>
-    </main>
+        <div>
+            <main className={styles.main}>
+                <h2 className={styles.title}>Home</h2>
+                <div className={styles.homeContent}>
+                    <div className={styles.infoGamesContainer}>
+                        <div className={styles.infoContainer}>
+                            <div className={styles.infoContent}>
+                                <h3 className={styles.contentTitle}>Account info</h3>
+                                <span className={styles.contentItem}>Username</span>
+                                <span className={styles.contentItem}>Credits</span>
+                                <span className={styles.contentItem}>Ranking</span>
+                            </div>
+                        </div>
+                        <div className={styles.gamesContainer}>
+                            <h3 className={styles.contentTitle}>Games</h3>
+                            <div className={styles.gamesContent}>
+                                <span className={styles.contentItem}>Ongoing games</span>
+                                <span className={styles.contentItem}>My events</span>
+                            </div>
+                            <button className={styles.homePageButton}>View more</button>
+                        </div>
+                    </div>
+                    <div className={styles.inventoryContent}>
+                        <h3 className={styles.contentTitle}>Inventory</h3>
+                        <div className={styles.inventoryContent}>
+                            <span className={styles.contentItem}>Total cards</span>
+                            <span className={styles.contentItem}>Total packs</span>
+                        </div>
+                        <button className={styles.homePageButton}>View more</button>
+                    </div>
+
+                </div>
+            </main>
+        </div>
     )
 }
 
