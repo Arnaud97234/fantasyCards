cc0f68cda67c7e5a04914a8c424a81eb5faf77ce
elie <elie.marot@gmail.com>
2023-12-12 18:30:43 +0100

    coponant card

---
+++ frontend/pages/market.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+import Market from '../components/Market'
+
+function marketPage() {
+  return (
+    <>
+        <Header />
+        <Market />
+    </>
+  )
+}
+
+export default marketPage
