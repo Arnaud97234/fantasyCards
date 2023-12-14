89c7cbe93ac5a9770307b890da5293850d3a1c8d
Maxence <maxence.ogier@gmail.com>
2023-12-14 17:21:05 +0100

    travaille foutu

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -2,7 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 require("../models/connection");
+const User = require("../models/users");
 const Event = require("../models/events");
+const Game = require("../models/games");
+const Team = require("../models/teams");
 
 router.post("/", (req, res) => {
   // Check if event is not already registered
@@ -13,7 +16,11 @@ router.post("/", (req, res) => {
         startDate: req.body.startDate,
         endDate: req.body.endDate,
         status: req.body.status,
-        gameId: ["6576fd7ad9e95bbd54710ad2", "6576fd7ad9e95bbd54710ad0", "6576fd7ad9e95bbd54710ad1"],
+        gameId: [
+          "6576fd7ad9e95bbd54710ad2",
+          "6576fd7ad9e95bbd54710ad0",
+          "6576fd7ad9e95bbd54710ad1",
+        ],
       });
       newEvent.save().then((newDoc) => {
         res.json({ result: true, dataGame: newDoc });
@@ -26,15 +33,50 @@ router.post("/", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  Event.find()
-    .then((events) => {
-      if (events === null) {
-        res.json({ result: false, error: "Events not found" });
-        return;
-      } else {
-        res.json({ result: true, events });
-      }
-    });
-});;
+  Event.find().then((events) => {
+    if (events === null) {
+      res.json({ result: false, error: "Events not found" });
+      return;
+    } else {
+      res.json({ result: true, events });
+    }
+  });
+});
+
+// router.get("/infoMyEvents/:token", (req, res) => {
+//   User.findOne({ token: req.params.token })
+//     .populate("eventsId cardsId")
+//     .exec()
+//     .then((userInfo) => {
+//       Event.find({ _id: userInfo.eventsId })
+//         .populate("gameId")
+//         .lean()
+//         .then((eventsInfo) => {
+//           console.log(eventsInfo);
+//           const game = eventsInfo.map((data, i)=> {
+//             return data.gameId
+//           })
+//           console.log(game); 
+//           const team = game.map((data, i)=> {
+//             return {game: i+1, teamHomeId: data.teamHomeId, teamAwayId: data.teamAwayId}
+//           })
+//           console.log(team);
+
+
+//           Team.find({
+//             $or: [{id: team[i].teamHomeId}, {id: team[i].teamAwayId}]
+//           })
+//           .then((teamInfo)=> {
+//             console.log(teamInfo);
+//           })
+//           res.json({
+//             result: true,
+//             userInfo,
+//             eventsInfo,
+//             teamInfo,
+//           });
+//         });
+//     });
+// });
 
 module.exports = router;
