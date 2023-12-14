63f47c698c4bd7f7113074e53f9b7ab0634174c4
elie <elie.marot@gmail.com>
2023-12-14 12:20:25 +0100

    fix_css_market

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,27 +1,36 @@
 import styles from '../styles/Card.module.css'
 
 
+
 function Card(props) {
+
+    // console.log(props.name)
+    // console.log('hello')
+    console.log(props.stock)
 
     return (
         <main>
-            <div className={styles.card}>
+            <div className={styles.card1}>
 
-                <div>
-                    <img style={{ width: 400, height: 60, minWidth: 35, maxWidth: 45 }} className='boxPicture' src='https://media-4.api-sports.io/football/players/647.png' alt='Card picture' />
-                    <h3>{props.name}</h3>
+                <div className={styles.card2}>
 
-                    <div className={styles.cardsymbol}>
-                        <img style={{ width: 15, height: 15, minWidth: 15, maxWidth: 15 }} className='boxPicture' src='https://media-4.api-sports.io/football/teams/81.png' alt='Team logo' />
-                        <img style={{ width: 15, height: 15, minWidth: 15, maxWidth: 15 }} className='boxPicture' src='./images/boxPicture_3.png' alt='Country' />
+                    <div>
+                        <img style={{ width: 400, height: 60, minWidth: 35, maxWidth: 45 }} className='boxPicture' src='https://media-4.api-sports.io/football/players/647.png' alt='Card picture' />
+                        <h3>{props.name}</h3>
+
+                        <div className={styles.cardsymbol}>
+                            <img style={{ width: 15, height: 15, minWidth: 15, maxWidth: 15 }} className='boxPicture' src='https://media-4.api-sports.io/football/teams/81.png' alt='Team logo' />
+                            <img style={{ width: 15, height: 15, minWidth: 15, maxWidth: 15 }} className='boxPicture' src='./images/boxPicture_3.png' alt='Country' />
+                        </div>
                     </div>
+
                 </div>
 
-            </div>
+                <div className={styles.cardButtonContainer}>
+                    <button className={styles.cardButton}>DESC</button>
+                    <button className={styles.cardButton}>BUY</button>
+                </div>
 
-            <div className={styles.cardButtonContainer}>
-                <button className={styles.cardButton}>DESC</button>
-                <button className={styles.cardButton}>BUY</button>
             </div>
         </main>
 
--- frontend/components/Market.js
+++ frontend/components/Market.js
@@ -1,43 +1,72 @@
 import styles from '../styles/Market.module.css'
 import Card from '../components/Card'
+// TODO : faire en sorte que 2 componante s'affiche sur la page
+import { useState, useEffect } from 'react';
 
 
 function Market(props) {
 
+    const [cardsData, setcardsData] = useState([]);
 
-    const cardsData = [
-        { teamId: 6451515, name: 'HANS SAMA', rarity: 'newyorker.jpg', stock: 2, picture: "https//" },
+    // ./images/boxPicture_3.png
+    const cards = [
+        { teamId: 6451515, name: 'HANS SAMA', rarity: '1', stock: 2, picture: "./images/boxPicture_1.png" },
+        { teamId: 45451545, name: 'HANS', rarity: '2', stock: 9, picture: "./images/boxPicture_2.png" },
+        { teamId: 6451515, name: 'HANS SAMA', rarity: '1', stock: 2, picture: "./images/boxPicture_1.png" },
+        { teamId: 45451545, name: 'HANS', rarity: '2', stock: 9, picture: "./images/boxPicture_2.png" },
+        { teamId: 6451515, name: 'HANS SAMA', rarity: '1', stock: 2, picture: "./images/boxPicture_1.png" },
+        { teamId: 45451545, name: 'HANS', rarity: '2', stock: 9, picture: "./images/boxPicture_2.png" },
+        { teamId: 6451515, name: 'HANS SAMA', rarity: '1', stock: 2, picture: "./images/boxPicture_1.png" },
+        { teamId: 45451545, name: 'HANS', rarity: '2', stock: 9, picture: "./images/boxPicture_2.png" },
+        { teamId: 6451515, name: 'HANS SAMA', rarity: '1', stock: 2, picture: "./images/boxPicture_1.png" },
+        { teamId: 45451545, name: 'HANS', rarity: '2', stock: 9, picture: "./images/boxPicture_2.png" },
+        { teamId: 6451515, name: 'HANS SAMA', rarity: '1', stock: 2, picture: "./images/boxPicture_1.png" },
+        { teamId: 45451545, name: 'HANS', rarity: '2', stock: 9, picture: "./images/boxPicture_2.png" },
       ];
 
 
-    const cards = cardsData.map((data, i) => {
-        return <Card key={i} name={data.name} />;
+    // console.log(cards[0])
     
+
+    useEffect(() => {
+
+        setcardsData(cards)
+        // console.log(cardsData)
+    })
+
+
+    const cardsmap = cardsData.map((data, i) => {
+        // console.log(data.stock)
+        return <Card key={i} teamId={data.teamId} name={data.name} rarity={data.rarity} stock={data.stock} picture={data.picture}/>;
+        
       });
 
+      
     return (
     <main className={styles.main}>
         <h1 className={styles.boxTitle1} >FANTASY MARKET</h1>
         <div className={styles.marketContainer}>
+            
             <div className={styles.content}>
+
                 <h3 className={styles.boxTitle2} >Buy Cards</h3>
-                <div>
-                    <Card/>
-                    {/* <img style={{ width: 1600, height: 250, minWidth: 150, maxWidth: 180 }} className='boxPicture' src='./images/boxPicture_3.png' alt='Create your team' />
-                    <div className={styles.cardButtonContainer}>
-                    <button className={styles.cardButton}>DESC</button>
-                    <button className={styles.cardButton}>BUY</button>
-                    </div> */}
-                </div>
+                
+                {cardsmap}
+                {/* <img style={{ width: 1600, height: 250, minWidth: 150, maxWidth: 180 }} className='boxPicture' src='./images/boxPicture_3.png' alt='Create your team' />
+                <div className={styles.cardButtonContainer}>
+                <button className={styles.cardButton}>DESC</button>
+                <button className={styles.cardButton}>BUY</button>
+                </div> */}
+               
             </div >
 
             
             <div className={styles.content}>
 
-            <div className={styles.divider}></div> 
+            {/* <div className={styles.divider}></div>  */}
                 
                 <h3 className={styles.boxTitle2}>Buy Packs</h3>
-                <Card/>
+                {cardsmap}
             </div>
         </div>
     </main>
